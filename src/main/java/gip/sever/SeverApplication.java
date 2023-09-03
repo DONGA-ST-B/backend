7db4a12ab9f5a56f35eb4b6753a774de8c8696d8
Bae Hyeonseo <haulqogustj@naver.com>
2023-09-03 23:53:34 +0900

    여기에 커밋 메시지 작성

---
+++ src/main/java/gip/sever/SeverApplication.java
@@ -0,0 +1,13 @@
+package gip.sever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeverApplication {
+//c
+	public static void main(String[] args) {
+		SpringApplication.run(SeverApplication.class, args);
+	}
+
+}
