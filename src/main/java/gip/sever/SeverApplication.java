6807702881eb788b720e72c7cfdf3728408d3374
Bae Hyeonseo <haulqogustj@naver.com>
2023-09-10 23:23:23 +0900

    TEST

--- src/main/java/gip/sever/SeverApplication.java
+++ src/main/java/gip/sever/SeverApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SeverApplication {
-//test311
+//test3
 	public static void main(String[] args) {
 		SpringApplication.run(SeverApplication.class, args);
 	}
