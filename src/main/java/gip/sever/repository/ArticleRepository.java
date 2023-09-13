6ba736fbe73bec65be0f4a50413002a06c7b57fa
정 승 연 <53565255+seungyeonnnnnni@users.noreply.github.com>
2023-09-14 04:40:57 +0900

    feat : 기사 엔티티 생성

---
+++ src/main/java/gip/sever/repository/ArticleRepository.java
@@ -0,0 +1,15 @@
+package gip.sever.repository;
+
+import gip.sever.domain.Article;
+import gip.sever.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+//    @Query("select p from Product p where p.productName like :keyword")
+//    Optional<List<Article>> findByKeyword(String keyword);
+}
