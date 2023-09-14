60c1eb2520958183e45ab5a55f2a3b8f3c483506
정 승 연 <53565255+seungyeonnnnnni@users.noreply.github.com>
2023-09-15 00:03:26 +0900

    fix : cartItemRepository 쿼리문 추가

--- src/main/java/gip/sever/repository/CartItemRepository.java
+++ src/main/java/gip/sever/repository/CartItemRepository.java
@@ -1,13 +1,17 @@
 package gip.sever.repository;
 
 import gip.sever.domain.CartItem;
+import org.hibernate.annotations.Parameter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-    List<CartItem> findByMemberIdAndCartId(Long memberId, Long cartId);
+    @Query("select ci from CartItem ci where ci.cart.member.id = :memberId and ci.cart.id =:cartId ")
+    List<CartItem> findByMemberIdAndCartId( @Param(value = "memberId") Long memberId, @Param(value = "cartId") Long cartId);
 }
 
